cb2de97c8d7184c41f828dd72adcf38dfc0b1689
tawbazf <tawbazehaf@gmail.com>
2025-02-27 09:51:49 +0100

    class income

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Income extends Model
-{
-    //
+class Income extends Model {
+    protected $fillable = ['user_id', 'amount', 'description', 'date'];
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
