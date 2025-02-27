a4cd854a6ae1123f4f4d7adbf37704cf44b990c8
tawbazf <tawbazehaf@gmail.com>
2025-02-27 09:58:15 +0100

    profile routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 
 // Registration
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
@@ -13,6 +14,14 @@ Route::post('/login', [AuthController::class, 'login']);
 // Logout
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+
+
+// Profile routes
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+});
 // Route::get('/', function () {
 //     return view('welcome');
 // });
