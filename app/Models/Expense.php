c3eb450ef0ca24e8a4c76dfec7673135376113b0
tawbazf <tawbazehaf@gmail.com>
2025-02-27 09:52:13 +0100

    class expense

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Expense extends Model
-{
-    //
+class Expense extends Model {
+    protected $fillable = ['user_id', 'category_id', 'amount', 'description', 'date'];
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
