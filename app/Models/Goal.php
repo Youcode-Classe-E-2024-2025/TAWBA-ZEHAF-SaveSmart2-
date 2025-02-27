7cccbac1d69302da23f04128ba2f12e77ec12d14
tawbazf <tawbazehaf@gmail.com>
2025-02-27 09:53:23 +0100

    class goal

--- app/Models/Goal.php
+++ app/Models/Goal.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Goal extends Model
-{
-    //
+class Goal extends Model {
+    protected $fillable = ['user_id', 'name', 'target_amount', 'current_amount', 'deadline'];
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
