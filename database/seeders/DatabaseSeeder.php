b9637621982f86d304ab2710d3a877af74df3917
tawbazf <tawbazehaf@gmail.com>
2025-03-06 10:27:05 +0100

    database seeder mofidication

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,23 +1,23 @@
 <?php
+ namespace Database\Seeders;
 
-namespace Database\Seeders;
-
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'tawba',
-            'email' => 'tawba@gmail.com',
-        ]);
-    }
+public function run(): void
+{
+DB::table('users')->insert([
+'name' => 'tawba',
+'email' => 'tawba@gmail.com',
+'email_verified_at' => now(), 
+'password' => bcrypt('password'),
+'remember_token' => Str::random(10),
+'created_at' => now(),
+'updated_at' => now(),
+]);
+}
 }
